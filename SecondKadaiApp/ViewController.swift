ffe609f780f10b49bbd1bd9d9ecddd2b7d7823a2
shunichiabe <shunichi5127@gmail.com>
2021-04-06 14:59:58 +0300

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 阿部俊一 on 2021/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
