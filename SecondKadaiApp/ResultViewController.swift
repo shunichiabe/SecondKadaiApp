45ba9ea2d50418752cffc294bfed848bd6aaebe7
shunichiabe <shunichi5127@gmail.com>
2021-04-09 14:43:32 +0300

    second Ikadai

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 阿部俊一 on 2021/04/06.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    var x:String = " "
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+
+        label.text = "こんにちは\(x)さん"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var Name: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        resultViewController.x = Name.text!
+    
+    }
+    
 
 }
 
